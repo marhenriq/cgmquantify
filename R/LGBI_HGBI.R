5aaaf2f4aa01aaed7a6118a7ab2fe2b04d0d9a53
Maria Henriquez <marhenriq@gmail.com>
2020-12-21 10:51:38 -0500

    LGBI_HGBI functions

---
+++ R/LGBI_HGBI.R
@@ -0,0 +1,45 @@
+LGBI_HBGI <- function(df) {
+  f = data.frame(log_glucose = log(df$glucose^1.084) - 5.381)
+  f <- mutate(f,
+              rl = case_when(
+                log_glucose <= 0 ~
+                  22.77*(log_glucose^2),
+                TRUE ~ 0),
+              rh = case_when(
+                log_glucose > 0 ~
+                  22.77*(log_glucose^2),
+                TRUE ~ 0)
+  )
+
+  LGBI = mean(f$rl)
+  HGBI = mean(f$rh)
+
+  return(data.frame(LGBI, HGBI))
+
+}
+
+
+LGBI <- function(df) {
+  f = data.frame(log_glucose = log(df$glucose^1.084) - 5.381)
+  f <- mutate(f,
+              rl = case_when(
+                log_glucose <= 0 ~
+                  22.77*(log_glucose^2),
+                TRUE ~ 0))
+
+  return(mean(f$rl))
+
+}
+
+
+HGBI <- function(df) {
+  f = data.frame(log_glucose = log(df$glucose^1.084) - 5.381)
+  f <- mutate(f,
+              rh = case_when(
+                log_glucose > 0 ~
+                  22.77*(log_glucose^2),
+                TRUE ~ 0))
+
+  return(mean(f$rh))
+
+}
